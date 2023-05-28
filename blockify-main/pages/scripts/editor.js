529d3b5342e1c6eef0ba29276c98ed228572339f
MeBadDev <94725241+MeBadDev@users.noreply.github.com>
2023-05-28 10:51:32 +0800

    fixed toolbox

--- blockify-main/pages/scripts/editor.js
+++ blockify-main/pages/scripts/editor.js
@@ -1,9 +1,24 @@
 import Blockly from 'blockly';
-var toolbox = document.getElementById("toolbox");
+var toolbox = document.getElementById("toolbox-xml");
+
+const theme = Blockly.Theme.defineTheme('blockify-classic', {
+	'base': Blockly.Themes.Classic,
+	'blockStyles': {
+	   'logic_block	s': {
+		//   'colourPrimary': '#4a148c'
+	   },
+	   'math_blocks': {}
+	},
+	'categoryStyles': {},
+	'componentStyles': {},
+	'fontStyle': {size:13, family: "sans"},
+	'startHats': true
+ });
 
 var options = { 
-	toolbox : toolbox, 
-	collapse : true, 
+	toolbox : toolbox,
+	theme:theme,
+	collapse : false, 
 	comments : true, 
 	disable : true, 
 	maxBlocks : Infinity, 
@@ -13,8 +28,8 @@ var options = {
 	css : true, 
 	media : 'https://blockly-demo.appspot.com/static/media/', 
 	rtl : false, 
-	scrollbars : true, 
-	sounds : true, 
+	scrollbars : false, 
+	sounds : true,
 	oneBasedIndex : true, 
 	grid : {
 		spacing : 32, 
@@ -24,13 +39,38 @@ var options = {
 	}
 };
 
+class CustomCategory extends Blockly.ToolboxCategory {
+	/**
+	* Constructor for a custom category.
+	* @override
+	*/
+	constructor(categoryDef, toolbox, opt_parent) {
+		super(categoryDef, toolbox, opt_parent);
+	}
+}
+Blockly.registry.register(
+	Blockly.registry.Type.TOOLBOX_ITEM,
+	Blockly.ToolboxCategory.registrationName,
+	CustomCategory, true);
+
+Blockly.Blocks['on_ready'] = {
+	init: function() {
+		this.appendStatementInput("block")
+		.setCheck(null)
+		.appendField("On Ready");
+		this.setColour(150);
+		this.setTooltip("Runs when this component is ready.");
+		this.setHelpUrl("");
+	}
+};
+
 /* Inject your workspace */ 
 var workspace = Blockly.inject("blockly-workspace", options);
-
 /* Load Workspace Blocks from XML to workspace. Remove all code below if no blocks to load */
 
 /* TODO: Change workspace blocks XML ID if necessary. Can export workspace blocks XML from Workspace Factory. */
 var workspaceBlocks = document.getElementById("toolbox-xml"); 
-
 /* Load blocks to workspace. */
 Blockly.Xml.domToWorkspace(workspaceBlocks, workspace);
+
+
